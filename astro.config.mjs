8977a477de213165727a53eccc26515b061a7719
tornkjoest <111287626+tornkjoest@users.noreply.github.com>
2024-12-22 19:48:55 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://tornkjoest.github.io",
+  base: "everleigh-story",
   integrations: [tailwind(), icon()],
 });
